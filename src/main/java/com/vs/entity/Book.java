d88e998a4febfd3d3c0cac2fabcbb2e05475c9ae
Vinay Sonekar <vinayso712@gmail.com>
2025-11-10 17:09:26 +0530

    removed unused imports

--- src/main/java/com/vs/entity/Book.java
+++ src/main/java/com/vs/entity/Book.java
@@ -1,13 +1,10 @@
 package com.vs.entity;
 
-import jakarta.annotation.Generated;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 //@Data
--- src/main/java/com/vs/util/GlobalExceptionHandler.java
+++ src/main/java/com/vs/util/GlobalExceptionHandler.java
@@ -7,13 +7,10 @@ import org.springframework.http.HttpStatus;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import feign.FeignException;
 
 
 @RestControllerAdvice
